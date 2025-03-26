dd12d4ea28c7437fc6cf70b82b3851eb0dcc2593
anushkaweavers <anushka.goswami@weavers-web.com>
2025-03-26 14:27:28 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // =============================================
 // ENVIRONMENT VARIABLE DIAGNOSTICS
 // =============================================
@@ -117,32 +116,3 @@ io.on("connection", (socket) => {
     socket.leave(userData._id);
   });
 });
-=======
-const express = require("express");
-const connectDB = require("./config/db");
-const dotenv = require("dotenv");
-const userRoutes = require("./routes/userRoutes");
-const chatRoutes = require("./routes/chatRoutes");
-const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-
-dotenv.config();
-connectDB();
-const app = express();
-
-app.use(express.json()); // to accept json data
-
-app.get("/", (req, res) => {
-  res.send("API Running!");
-});
-
-app.use("/api/user", userRoutes);
-app.use("/api/chat", chatRoutes);
-
-// Error Handling middlewares
-app.use(notFound);
-app.use(errorHandler);
-
-const PORT = process.env.PORT;
-
-app.listen(PORT, console.log(`Server running on PORT ${PORT}...`.yellow.bold));
->>>>>>> 3ac5545 (Authentication DOne)
